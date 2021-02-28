8d57eb1254d2222983da87e128d837b1330cbb95
Denis Bondar <60221683+Dunno-official@users.noreply.github.com>
2021-02-28 20:07:28 +0200

    Newton's method

--- Lab1 Equation solver/Newton's method/Newton's method/main.cpp
+++ Lab1 Equation solver/Newton's method/Newton's method/main.cpp
@@ -13,26 +13,57 @@ using namespace std;
          -               +
 	 ---------  4/3  ---------
 
-	f(-0.8) = 1
-	f(-2) = -12
+	f(0) = 6
+	f(-2) = -19
+	A = [-2 , 0]
 
-	f(-0.8) * f(-2) < 0
 
-	A = [-2 , -0.8]
+	1) f(x) ∈ C^2(A)     
+	2) f(0) * f(-2) < 0   ]  
 
 	∃ x ∈ A  :  f(x) = 0
-	f'(x) > 0   x ∈ A
-	f''(x) < 0  x ∈ A
 
+	3) f''(x) doesn't change sign on A
+	
+	Let x0 = -1.5
 
-	m1 = min |f'(x)| x ∈ A  = 4.12
-	M1 = max |f'(x)| x ∈ A  = 19
+	4) f(-1.5) * f''(-1.5) > 0
 
-	t_op = 2 / (m1 + M1) = 0.0865
+	1-4) conditions are met for using Newton's method
 */
+
+
+long double f(long double x);
+long double f_derivative(long double x);
 
 
 int main()
 {
+	long double x = -1.5;
+	long double f_der2 = f_derivative(-1.5);
 
+	const int iterations = 12;
+
+	for (int i = 0; i < iterations; ++i)
+	{
+		cout << "x" << i << " = " << x << endl;
+		x = x - f(x) / f_der2;
+	}
+
+	cout << "x" << iterations << " = " << x << endl;
+
+	return 0;
 }
+
+
+long double f(long double x)
+{
+	return powl(x, 3) - 4 * powl(x, 2) + x + 6;
+}
+
+
+long double f_derivative(long double x)
+{
+	return 3 * powl(x, 2) - 8 * x + 1;
+}
+
